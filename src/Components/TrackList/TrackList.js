63a96ad03dc5d735ce08c51761d078f9c3d77cf5
rucpata <rucinska.patrycja@gmail.com>
2021-03-22 14:15:00 +0100

    created static components

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track.js'
+
+class TrackList extends React.Component{
+    render(){
+        return(
+            <div className="TrackList">
+                <Track/>
+            </div>
+        )
+    }
+}
+
+export default TrackList;
