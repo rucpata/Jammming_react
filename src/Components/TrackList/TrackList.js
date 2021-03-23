7157915abc04c43e159341959c8470758a187905
rucpata <rucinska.patrycja@gmail.com>
2021-03-23 16:28:13 +0100

    add Tracks to a Playlist

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -8,7 +8,7 @@ class TrackList extends React.Component{
         return(
             <div className="TrackList">
                 {this.props.tracks.map(track => {
-                    return <Track key={track.id} track={track}/>
+                    return <Track key={track.id} track={track} onAdd={this.props.onAdd}/>
                     })
                 }
             </div>
