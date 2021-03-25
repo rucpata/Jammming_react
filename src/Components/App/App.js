2fdeef2c6d5aa018454e620c34364fd037e78139
rucpata <rucinska.patrycja@gmail.com>
2021-03-25 22:55:22 +0100

    Save a Users Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,20 +5,17 @@ import SearchBar from '../SearchBar/SearchBar.js';
 import SearchResults from '../SearchResults/SearchResults.js';
 import Playlist from '../Playlist/Playlist.js';
 
-import Spotify from '../../util/Spotify';
+import Spotify from '../../util/Spotify.js';
 
 
 class App extends React.Component {
   constructor(props){
     super(props);
+    
     this.state = {
-      searchResults: [
-        {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
-        {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
-        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
-        playlistName: 'My Playlist',
-        playlistTracks: [
-          {name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4}, {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5}, {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
+      searchResults: [],
+      playlistName: 'My Playlist',
+      playlistTracks: []
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -50,8 +47,13 @@ class App extends React.Component {
   }
 
   savePlaylist(){
-    alert('działa')
     const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    })
   }
 
   search(term){
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
-const clientId = 'e898edfadc744b9c9de0e3d4a2f93a42';
-const redirectUri = 'http://localhost:3000/';
+const clientId = `e898edfadc744b9c9de0e3d4a2f93a42`;
+const redirectUri = `http://localhost:3000/`;
 let accessToken;
 
 const Spotify = {
@@ -18,9 +18,11 @@ const Spotify = {
 
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
+            return accessToken;
 
         } else {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+
             window.location = accessUrl;
         }
     },
@@ -28,9 +30,8 @@ const Spotify = {
     search(term){
         const accessToken = Spotify.getAccessToken();
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, 
-        {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
+        {headers: {
+            Authorization: `Bearer ${accessToken}`
             }
         }).then(response => {
             return response.json();
@@ -44,8 +45,39 @@ const Spotify = {
                 artist: track.artists[0].name,
                 album: track.album.name,
                 url: track.uri
-            }))
+            }));
+        });
+    },
+
+    savePlaylist(name, trackUris){
+        if(!name || !trackUris.length){
+            return;
+        }
+
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}`};
+        let userId;
+
+        return fetch(`https://api.spotify.com/v1/me`, {headers: headers}
+        ).then(response => response.json()
+        ).then(jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({name: name})
+            })
+            }).then(response => response.json()
+            ).then(jsonResponse => {
+                const playlistId  = jsonResponse.id; 
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({uris: trackUris})
+            })
+
         })
+        
     }
 }
 
